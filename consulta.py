45b351ad93255463d23bc8fbd70afaead9df0455
ArthurCorreia213 <arthurcnascimento132@gmail.com>
2025-07-31 16:23:07 -0300

    Commit inicial

---
+++ consulta.py
@@ -0,0 +1,14 @@
+import os
+import requests
+
+api_key = os.getenv('API_KEY')
+
+cot = requests.get(f"https://v6.exchangerate-api.com/v6/{api_key}/latest/USD").json()
+
+conv = []
+
+conv.append(cot["conversion_rates"])
+
+print("As cotações do USD são as seguintes:")
+for k,v in conv[0].items():
+    print(k,v)
